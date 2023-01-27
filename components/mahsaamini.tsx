e7a830f24c06d79df1869fd225d097d223a1f118
sinapirani <realsinapirani@gmail.com>
2023-01-27 13:50:49 +0330

    📦: Add Sinapirani label || add Oswald font

---
+++ components/mahsaamini.tsx
@@ -0,0 +1,7 @@
+export const MahsaAmini = () => {
+  return (
+    <div className="mb-4">
+      <p className=" font-[Oswald]">made by <a className=" text-yellow-400" target={"_blank"} href="https://github.com/sinapirani">Sinapirani</a> for #MahsaAmini</p>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { SearchInput } from '../components/searchInput'
 import { Results } from '../components/results'
+import { MahsaAmini } from '../components/mahsaamini'
+import { MadeBy } from '../components/mademy'
 
 
 export default function Home() {
   return (
     <div className=" text-white min-h-[100vh] w-full flex flex-col justify-center items-center gap-y-8">
+  
       <SearchInput/>
       <Results/>
-      <div className='mb-4'>
-        <p className=' font-sans'>#MahsaAmini</p>
-      </div>
+      <MahsaAmini/>
     </div>
   )
 }
